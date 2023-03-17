dfc148c6adcd645275de14fd1d92cad7f03069ce
Souleimane LEVASLOT <slevaslo@e2r3p5.clusters.42paris.fr>
2023-03-17 17:06:10 +0100

    hey

---
+++ pipex_bonus/pipex.c
@@ -0,0 +1,121 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: slevaslo <slevaslo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/25 14:48:01 by slevaslo          #+#    #+#             */
+/*   Updated: 2023/03/15 16:56:11 by slevaslo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+void	exec_process(char *str, char **envp)
+{
+	char	**mycmdargs;
+	char	*path;
+
+	mycmdargs = ft_split(str, ' ');
+	if (!mycmdargs)
+		return (ft_freetab(mycmdargs));
+	path = find_path(mycmdargs[0], envp);
+	if (!path)
+		not_find(mycmdargs);
+	execve(path, mycmdargs, envp);
+}
+
+void	dupncloses(int i, int pipes[2], t_data *data)
+{
+	if (i == 0)
+	{
+		dup2(data->fd_in, STDIN_FILENO);
+		dup2(pipes[1], STDOUT_FILENO);
+		close(data->fd_in);
+		if (data->is_here_doc == 1)
+			close(data->here_doc[0]);
+		close(pipes[1]);
+	}
+	else if (i == data->cmds - 1)
+	{
+		dup2(data->prev, STDIN_FILENO);
+		dup2(data->fd_out, STDOUT_FILENO);
+		close(data->prev);
+	}
+	else
+	{
+		dup2(data->prev, STDIN_FILENO);
+		dup2(pipes[1], STDOUT_FILENO);
+		close(pipes[1]);
+	}
+	close(pipes[0]);
+}
+
+void	savencloses(int i, int pipes[2], t_data *data)
+{
+	if (i != data->cmds - 1)
+	{
+		if (data->prev != -1)
+			close(data->prev);
+		data->prev = pipes[0];
+	}
+	else
+	{
+		close(data->prev);
+		close(pipes[0]);
+	}
+}
+
+void	process(char **argv, char **envp, t_data *data)
+{
+	int	i;
+	int	pid;
+	int	pipes[2];
+
+	i = 0;
+	while (i < data->cmds)
+	{
+		pipe(pipes);
+		pid = fork();
+		if (pid == -1)
+			error();
+		else if (pid == 0)
+		{
+			dupncloses(i, pipes, data);
+			exec_process(argv[i + (2 + data->is_here_doc)], envp);
+		}
+		close(pipes[1]);
+		savencloses(i, pipes, data);
+	i++;
+	}
+	waitpid(pid, NULL, 0);
+}
+
+int	main(int argc, char **argv, char **envp)
+{
+	t_data	data;
+
+	if (argc < 5)
+	{
+		ft_putstr_fd("\033[31;5mError\033[0m Usage: ", 1);
+		ft_putstr_fd("<infile> cmd1 | cmd2 | ... | cmdN > <outfile>\n", 1);
+		exit(EXIT_FAILURE);
+	}
+	data.prev = -1;
+	data.fd_out = open(argv[argc - 1], O_WRONLY | O_CREAT | O_TRUNC, 0666);
+	if (ft_strncmp(argv[1], "here_doc", 8) == 0)
+		here_doc(argv[2], &data);
+	else
+	{
+		data.fd_in = open(argv[1], O_RDONLY);
+		data.is_here_doc = 0;
+	}
+	data.cmds = argc - (3 + data.is_here_doc);
+	if (data.fd_out == -1 || data.fd_in == -1)
+		error();
+	process(argv, envp, &data);
+	close(data.fd_in);
+	close(data.fd_out);
+	return (0);
+}
---
+++ pipex_bonus/pipex.h
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: slevaslo <slevaslo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/02 17:47:35 by slevaslo          #+#    #+#             */
+/*   Updated: 2023/03/15 16:56:21 by slevaslo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+# include "libft/libft.h"
+# include "get_next_line/get_next_line.h"
+# include <stddef.h>
+# include <stdlib.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+
+typedef struct data
+{
+	int	prev;
+	int	cmds;
+	int	fd_out;
+	int	fd_in;
+	int	here_doc[2];
+	int	is_here_doc;
+}t_data;
+
+void	exec_process(char *str, char **envp);
+void	not_find(char **mycmdargs);
+void	error(void);
+void	ft_freetab(char **str);
+char	*find_path(char *cmd, char **envp);
+char	*path_is_ok(char **paths, char *cmd);
+void	process(char **argv, char **envp, t_data *data);
+char	*ft_strjoin(char const *s1, char const *s2);
+void	here_doc(char *argv, t_data *data);
+#endif
